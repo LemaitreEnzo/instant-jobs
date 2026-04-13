d6d7d93d959c0963d51f213a7b7512e016fa7cc2
Lemaitre Enzo <lemaitreenzo05@gmail.com>
2026-04-13 15:28:56 +0200

    Update Dockerfile

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,8 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    host: "0.0.0.0",
+    port: Number(process.env.PORT),
     cors: true,
     allowedHosts: [
       "instant-jobs.duckdns.org",
