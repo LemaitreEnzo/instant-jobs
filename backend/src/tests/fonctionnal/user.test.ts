485143876850fbfcb483997886862a37e2960863
Lemaitre Enzo <lemaitreenzo05@gmail.com>
2026-04-14 10:18:13 +0200

    First commit

--- backend/src/tests/fonctionnal/user.test.ts
+++ backend/src/tests/fonctionnal/user.test.ts
@@ -3,27 +3,29 @@ import { UserModel } from "models/users.model";
 import request from "supertest";
 import app from "../../../app";
 
+// Create mock for user model
 jest.mock("models/users.model", () => ({
   UserModel: {
     findAll: jest.fn(),
     findOne: jest.fn(),
     create: jest.fn(),
     update: jest.fn(),
-    delete: jest.fn(),
+    destroy: jest.fn(),
   },
 }));
 
-describe("GET all users", () => {
+describe("GET USER", () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
-  it("should return 200", async () => {
+  it("Should return 200", async () => {
     const res = await request(app).get("/users");
     expect(res.status).toBe(200);
   });
 
-  it("Get all users", async () => {
+  it("Returns all users", async () => {
+    // Create mock for findAll fuction
     jest
       .mocked(UserModel.findAll)
       .mockResolvedValue([
@@ -39,5 +41,66 @@ describe("GET all users", () => {
         { id: 2, name: "eeee" },
       ],
     });
+  });
+
+  it("Returns one user", async () => {
+    jest
+      .mocked(UserModel.findOne)
+      .mockResolvedValue({ id: 1, name: "ee" } as any);
+
+    const res = await request(app).get("/users/1");
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      users: { id: 1, name: "ee" },
+    });
+  });
+});
+
+describe("CREATE ONE USER", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("Create one user", async () => {
+    jest
+      .mocked(UserModel.create)
+      .mockResolvedValue({ id: 1, name: "instant-jobs" } as any);
+
+    const res = await request(app)
+      .post("/users")
+      .send({ id: 1, name: "instant-jobs" });
+    expect(res.status).toBe(201);
+    expect(res.body).toMatchObject({
+      users: { id: expect.any(Number), name: "instant-jobs" },
+    });
+  });
+
+  it("Returns a 409 error if a user's email address is already in use.", async () => {
+    jest
+      .mocked(UserModel.findOne)
+      .mockResolvedValue({ id: 1, name: "instant-jobs" } as any);
+
+    const res = await request(app)
+      .post("/users")
+      .send({ id: 1, name: "instant-jobs" });
+
+    expect(res.status).toBe(409);
+    expect(UserModel.create).not.toHaveBeenCalled();
+  });
+});
+
+describe("DELETE ONE USER", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("Delete", async () => {
+    jest
+      .mocked(UserModel.destroy)
+      .mockResolvedValue({ id: 1, name: "instant-jobs" } as any);
+
+    const res = await request(app).delete("/users/1");
+    expect(res.status).toBe(200);
+    expect(UserModel.destroy).toHaveBeenCalled();
   });
 });
