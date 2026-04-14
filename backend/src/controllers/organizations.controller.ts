b6d419df39429b0fec77be45f648da2c60a23109
Florian Lavigne Chappaz <florian.lavigne.chappaz@gmail.com>
2026-04-14 16:07:57 +0200

    Implement CRUD operations for organizations; update routes, controller, model, and tests

--- backend/src/controllers/organizations.controller.ts
+++ backend/src/controllers/organizations.controller.ts
@@ -1,11 +1,13 @@
 import type { Request, Response } from "express";
 import { Organization } from "models/organizations.model";
+import { where } from "sequelize";
 
-export const getAllOrganizations = async (req: Request, res: Response) => {
-    try {
-        const organizations = await Organization.findAll();
-        res.status(200).json({ organizations });
-    } catch (error) {
-        res.status(500).json(error);
-    }
-}
+export const getAllOrganizations = async (req: Request, res: Response) => {};
+
+export const getOneOrganization = async (req: Request, res: Response) => {};
+
+export const createOrganization = async (req: Request, res: Response) => {};
+
+export const updateOrganization = async (req: Request, res: Response) => {};
+
+export const deleteOrganization = async (req: Request, res: Response) => {}
