277595cbe281a8df58dc25feb792c3113a696bec
Lemaitre Enzo <lemaitreenzo05@gmail.com>
2026-09-18 00:01:18 +0200

    fix: routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route
-          path="/:orgaSlug"
+          path="/"
           element={
             <ProtectedRoute>
               <Dashboard />
