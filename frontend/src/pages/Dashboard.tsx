a5b908008654a492b8dbf77110ab1cd96c878273
Descorsiers <desco.nico456@gmail.com>
2026-09-25 15:42:00 +0200

    feat/pageTitle

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,13 +1,10 @@
 import MainLayout from "../components/layout/MainLayout/MainLayout";
 import "../assets/css/pages/dashboard.css";
-import PageTitle from "../components/layout/PageTitle/PageTitle";
 
 function Dashboard() {
   return (
     <MainLayout>
-      <div className="dashboard">
-        < PageTitle title="test" />
-      </div>
+      <div className="dashboard"></div>
     </MainLayout>
   );
 }
